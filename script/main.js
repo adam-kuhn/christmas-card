ce406191f8be688b405193d94ccc7d7d509e72dc
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-17 22:24:23 +1300

    playing with css responsivness text

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
   introSong ()
   document.getElementById("rudolph").addEventListener('click', introSong)
 }
-
+//play christmas song
 function introSong(){
   intro = document.getElementById("intro-song")
   intro.play()
