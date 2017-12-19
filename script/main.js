994544330e0267787e6cb7ab01bad73ad7e1169d
adam-kuhn <adam.kuhn20@gmail.com>
2017-12-19 15:38:09 +1300

    add basic modal

--- script/main.js
+++ script/main.js
@@ -1,10 +1,40 @@
-/*window.onload = function () {
-  introSong ()
-  document.getElementById("rudolph").addEventListener('click', introSong)
+window.onload = function () {
+/*  introSong ()
+  document.getElementById("rudolph").addEventListener('click', introSong)*/
+
+  //Get modal
+  var modal = document.getElementById("giftModal-1");
+
+  //get button that opens modal
+  var gift1 = document.getElementById("gift-1")
+
+
+  //get the <span> element that close the Modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // whe the user clicks on the button, open Modal
+  gift1.onclick = function(){
+    modal.style.display = "block"
+  }
+
+  // when user clicks on <span> (x), close the Modal
+  span.onclick = function () {
+    modal.style.display = "none"
+  }
+
+  //when the user clicks anywhere outside the modal, close it
+  window.onclick = function(event){
+    if (event.target == modal) {
+      modal.style.display ="none"
+    }
+  }
+
+
 }
+
+
 //play christmas song
 function introSong(){
   intro = document.getElementById("intro-song")
   intro.play()
 }
-*/
